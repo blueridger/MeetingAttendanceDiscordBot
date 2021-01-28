a70cb0013896607e1ec86fc983962a4cf4af97c1
Thena Seer <nath.sears@gmail.com>
2021-01-28 20:14:48 +0000

    Fetch voice channel fresh, improved logging, modularization.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import time
 import traceback
 from keep_alive import keep_alive
+import threading
+from replit import db
 
 client = discord.Client()
 
@@ -15,7 +17,7 @@ KEY_PARTICIPANTS = 'participants:'.lower()
 
 REQUIRED_KEYS = [KEY_CHANNEL, KEY_LENGTH_MINS]
 OUTPUT_OMITTED_KEYS = [KEY_OUTPUT_CHANNEL]
-BUSY_WAIT_INTERVAL_SECONDS = 20
+BUSY_WAIT_INTERVAL_SECONDS = 15
 
 def is_int(s):
     try: 
@@ -42,89 +44,91 @@ async def on_message(message):
 
   if message.content.startswith('$meeting'):
     try:
-      startTime = datetime.utcnow()
 
       # Parse the command
-      result = {}
+      parsed_params = {}
       currentKey = None
       for param in message.content.split():
         if param.endswith(':'):
           currentKey = param.lower()
-        elif currentKey in result.keys() and currentKey not in REQUIRED_KEYS:
-          result[currentKey] = ' '.join([result[currentKey], param])
+        elif currentKey in parsed_params.keys() and currentKey not in REQUIRED_KEYS:
+          parsed_params[currentKey] = ' '.join([parsed_params[currentKey], param])
         elif currentKey:
-          result[currentKey] = param
+          parsed_params[currentKey] = param
       
       # Handle incorrect usage
-      if not all(key in result.keys() for key in REQUIRED_KEYS):
+      if not all(key in parsed_params.keys() for key in REQUIRED_KEYS):
         await message.author.send(content="A required key was not found. Please provide the keys: {requiredKeys}\n https://github.com/blueridger/MeetingAttendanceDiscordBot/blob/mainline/README.md".format(requiredKeys=REQUIRED_KEYS))
         return
-      if (not is_int(result[KEY_LENGTH_MINS]) or int(result[KEY_LENGTH_MINS]) < 1 or int(result[KEY_LENGTH_MINS]) > 120):
-        await message.author.send(content="{length_mins_key} [{length_mins}] was not valid. Please provide an integer between 1 and 120.".format(length_mins_key=KEY_LENGTH_MINS, length_mins=result[KEY_LENGTH_MINS]))
+      if (not is_int(parsed_params[KEY_LENGTH_MINS]) or int(parsed_params[KEY_LENGTH_MINS]) < 1 or int(parsed_params[KEY_LENGTH_MINS]) > 120):
+        await message.author.send(content="%s [%s] was not valid. Please provide an integer between 1 and 120." % (KEY_LENGTH_MINS, parsed_params[KEY_LENGTH_MINS]))
         return
 
       # Parse some helper variables
-      timeLimitSecs = int(result[KEY_LENGTH_MINS]) * 60
       voice_channel = None
       for channel in message.guild.voice_channels:
-        if result[KEY_CHANNEL] in channel.name:
+        if parsed_params[KEY_CHANNEL] in channel.name:
           voice_channel = channel
-          result[KEY_CHANNEL] = channel.name
+          parsed_params[KEY_CHANNEL] = channel.name
           break
 
       output_channel = message.channel
-      if KEY_OUTPUT_CHANNEL in result.keys():
+      if KEY_OUTPUT_CHANNEL in parsed_params.keys():
         for channel in message.guild.text_channels:
-          if result[KEY_OUTPUT_CHANNEL] in channel.name:
+          if parsed_params[KEY_OUTPUT_CHANNEL] in channel.name:
             output_channel = channel
-            result[KEY_OUTPUT_CHANNEL] = channel.name
+            parsed_params[KEY_OUTPUT_CHANNEL] = channel.name
             break
       
       # Handle more incorrect usage
       if (not voice_channel):
-        await message.author.send(content="Channel: [{channel}] was not found. Please provide a valid voice channel name.".format(channel=result[KEY_CHANNEL]))
+        await message.author.send(content="Channel: %s was not found. Please provide a valid voice channel name." % parsed_params[KEY_CHANNEL])
         return
       
       # Send the initial output
-      print('Starting to watch.')
-      meeting_message = await output_channel.send(format_message(result))
+      meeting_message = await output_channel.send(format_message(parsed_params))
+      startWatching(meeting_message, message.author, parsed_params, voice_channel.id)
 
-      # Watch and build the participants lists and update the output
-      participant_ids = set()
-      participants = set()
-      participant_names = set()
-      while datetime.utcnow() < startTime + timedelta(seconds=timeLimitSecs):
-        new_ids = set(voice_channel.voice_states.keys()) - participant_ids
-        users = [(await client.fetch_user(user_id)) for user_id in new_ids]
-        users = list(filter(lambda u: not u.bot, users))
-        participants = participants | set([user.mention for user in users])
-        participant_names = participant_names | set([user.name for user in users])
-        participant_ids = participant_ids | new_ids
-        result[KEY_PARTICIPANTS] = '(watching) ' + ' '.join(participants)
-        if len(new_ids) > 0:
-          print(new_ids)
-          await meeting_message.edit(content=format_message(result), suppress=True)
-        time.sleep(BUSY_WAIT_INTERVAL_SECONDS)
       
-      print('Finished watching.')
-
-      # Finalize outputs
-      result[KEY_PARTICIPANTS] = ' '.join(participants)
-      await meeting_message.edit(content=format_message(result))
-      roam_formatted = message.content + '\n```[[' + ']]\n[['.join(participant_names) + ']]```'
-      await message.author.send(content=roam_formatted)
     except discord.errors.NotFound:
       # Message was deleted
-      pass
+      print('[%s] Message was deleted.' % meeting_message.id)
     except discord.errors.HTTPException as e:
       print(e)
     except:
       traceback.print_exc()
       try:
         await message.author.send(content="An error was thrown. Please have the bot maintainer check the console log for details.")
-      except:
-        pass
-    
+      except Exception as e:
+        print(e)
+
+def startWatching(meeting_message, user, parsed_params, voice_channel_id, participant_ids = set(), participants = set(), participant_names = set()):
+  startTime = meeting_message.created_at
+  timeLimitSecs = int(parsed_params[KEY_LENGTH_MINS]) * 60
+  # Watch and build the participants lists and update the output
+  print('[%s] Starting to watch.' % meeting_message.id)
+  while datetime.utcnow() < startTime + timedelta(seconds=timeLimitSecs):
+    new_ids = set((await client.fetch_channel(voice_channel_id)).voice_states.keys()) - participant_ids
+    users = [(await client.fetch_user(user_id)) for user_id in new_ids]
+    users = list(filter(lambda u: not u.bot, users))
+    participants = participants | set([user.mention for user in users])
+    participant_names = participant_names | set([user.name for user in users])
+    participant_ids = participant_ids | new_ids
+    parsed_params[KEY_PARTICIPANTS] = '(watching) ' + ' '.join(participants)
+    if len(new_ids) > 0:
+      print('[%s] Adding %s' % (meeting_message.id, new_ids))
+      await meeting_message.edit(content=format_message(parsed_params), suppress=True)
+    time.sleep(BUSY_WAIT_INTERVAL_SECONDS)
+  
+  print('[%s] Finished watching.' % meeting_message.id)
+
+  # Finalize outputs
+  parsed_params[KEY_PARTICIPANTS] = ' '.join(participants)
+  await meeting_message.edit(content=format_message(parsed_params))
+  roam_formatted = '\n```[[' + ']]\n[['.join(participant_names) + ']]```'
+  await user.send(content=roam_formatted)
+  print('[%s] Finished thread.' % meeting_message.id)
+
 keep_alive()
 try:
   client.run(os.getenv('TOKEN'))
